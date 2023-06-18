1d81328e6f4fdd9055f88667e9a97855c4f6efd3
Ankit Porwal <96345105+AnkitPorwal04@users.noreply.github.com>
2023-06-19 00:34:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Home from "./components/Home";
 import Work from "./components/Work";
 import Timeline from "./components/Timeline";
 import Services from "./components/Services";
-import Testimonial from "./components/Testimonial";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import { Toaster } from "react-hot-toast";
@@ -32,7 +31,6 @@ function App() {
       <Work />
       <Timeline />
       <Services />
-      <Testimonial />
       <Contact />
       <Footer />
       <Toaster />
